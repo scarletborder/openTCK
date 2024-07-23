8bdce0f047c3b28840592067b0708cdceb42c6c5
scarletborder <baishuibeef@gmail.com>
2024-07-24 00:00:06 +0800

    feat: proto files for defination in link

---
+++ doc/reconnect.md
@@ -0,0 +1,3 @@
+# Reconnect function intro
+
+这是一次颠覆性更新,会较大的更改先前的大厅和建立连接的逻辑
---
+++ test/9-test_grpc_dir.py
@@ -0,0 +1,22 @@
+# src/test/test_main.py
+import sys
+import os
+import asyncio
+
+# 获取当前文件的目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 获取项目根目录
+project_root = os.path.abspath(os.path.join(current_dir, "../"))
+stub_root = os.path.abspath(os.path.join(project_root, "./src/utils/link/stub/"))
+
+# 将项目根目录添加到 sys.path
+sys.path.append(project_root)
+sys.path.append(stub_root)
+
+# 测试,import stub
+
+import src.utils.link.stub.gamelink_pb2 as gpb2
+
+aa = gpb2.LobbyPlayer(uid=10, name="he")
+print(aa)
